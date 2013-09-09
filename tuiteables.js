a50a53301460260e8a679b9924b3727acfa77108
jkusunoki <jkusunoki@gmail.com>
2013-09-09 14:28:42 -0300

    Contabilizando caracteres

--- tuiteables.js
+++ tuiteables.js
@@ -1,9 +1,8 @@
 var url = window.location;
 tuiteable = /: "([a-zA-Z0-9 ÑñÁÉÍÓÚáéíóú;.,]+)"/g;
-caracteres = tuiteable.length;
-window.alert(caracteres);
+var caracteres = tuiteable.toString();
 
-function linkTuiteable(text) {
+function linkTuiteable(text) { 
     return text.replace(
        tuiteable,
         ': <a class="tuiteable" href="https://twitter.com/share?url='+url+'&amp;text=$1">$1</a>'
@@ -12,6 +11,8 @@ function linkTuiteable(text) {
 
 $('#contenido').ready(function(){
     $('p').each(function() {
-        $(this).html(linkTuiteable($(this).html()));
+        if (caracteres.length < 100) {
+            $(this).html(linkTuiteable($(this).html()));
+        }
     });
 });
