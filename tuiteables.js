95359483f356e293ed25eef55403946fb3a8ca5d
jkusunoki <jkusunoki@gmail.com>
2013-09-09 15:52:15 -0300

    Agregado estilos de demo y html

--- tuiteables.js
+++ tuiteables.js
@@ -1,18 +1,20 @@
-var url = window.location;
-tuiteable = /: "([a-zA-Z0-9 ÑñÁÉÍÓÚáéíóú;.,]+)"/g;
-var caracteres = tuiteable.toString();
+$(document).ready(function() {
+	var url = window.location;
+	tuiteable = /: "([a-zA-Z0-9 ÑñÁÉÍÓÚáéíóú;.,]+)"/g;
+	var caracteres = tuiteable.toString();
 
-function linkTuiteable(text) { 
-    return text.replace(
-       tuiteable,
-        ': <a class="tuiteable" href="https://twitter.com/share?url='+url+'&amp;text=$1">$1</a>'
-    );
-} 
+	function linkTuiteable(text) { 
+	    return text.replace(
+	       tuiteable,
+	        ': <a class="tuiteable" href="https://twitter.com/share?url='+url+'&amp;text=$1">$1</a>'
+	    );
+	} 
 
-$('#contenido').ready(function(){
-    $('p').each(function() {
-        if (caracteres.length < 100) {
-            $(this).html(linkTuiteable($(this).html()));
-        }
-    });
+	$('#contenido').ready(function(){
+	    $('p').each(function() {
+	        if (caracteres.length < 100) {
+	            $(this).html(linkTuiteable($(this).html()));
+	        }
+	    });
+	});
 });
