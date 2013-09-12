e7e1ac4700d34b6121d83573facf67829be668f1
jkusunoki <jkusunoki@gmail.com>
2013-09-11 22:31:41 -0300

    blockquotes

--- tuiteables.js
+++ tuiteables.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-	var url = window.location;
+	url = window.location;
 	tuiteable = /: "([a-zA-Z0-9 ÑñÁÉÍÓÚáéíóú;.,]+)"/g;
-	var caracteres = tuiteable.toString();
+	caracteres = tuiteable.toString();
+	tuiteableq = $('blockquote').text();
+	caracteresq = tuiteableq.toString();
 
 	function linkTuiteable(text) { 
 	    return text.replace(
@@ -10,10 +12,22 @@ $(document).ready(function() {
 	    );
 	} 
 
+	function linkTuiteableq(text) { 
+	    return text.replace(
+	       tuiteableq,
+	        '<a class="quotation" href="https://twitter.com/share?url='+url+'&amp;text='+tuiteableq+'">'+tuiteableq+'</a>'
+	    );
+	} 
+
 	$('#contenido').ready(function(){
 	    $('p').each(function() {
 	        if (caracteres.length < 100) {
 	            $(this).html(linkTuiteable($(this).html()));
+	        }
+	    });
+	    $('blockquote').each(function() {
+	        if (caracteresq.length < 100) {
+	            $(this).html(linkTuiteableq($(this).html()));
 	        }
 	    });
 	});
